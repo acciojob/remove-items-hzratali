88c75c9c8c1045940d7da2eb1f3d2f4b30330ef6
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-03 23:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.querySelector('input[type="button"]');
+    button.addEventListener('click', function() {
+        var select = document.getElementById('colorSelect');
+        var selectedIndex = select.selectedIndex;
+        if (selectedIndex !== -1) {
+            select.remove(selectedIndex);
+        }
+    });
+});
